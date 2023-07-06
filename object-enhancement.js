97eefb64df005f5e61449115e2e43adcd676c09e
ghostchinchilla <133160254+ghostchinchilla@users.noreply.github.com>
2023-07-06 15:02:45 -0400

    Add files via upload

---
+++ object-enhancement.js
@@ -0,0 +1,38 @@
+//Same keys and values ES2015
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    }
+  }
+
+  //Computed Property Names ES2015
+let favoriteNumber = 42;
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+}
+
+//Object Methods ES2015
+const instructor = {
+    firstName: "Colt",
+    sayHi(){
+      return "Hi!";
+    },
+    sayBye(){
+      return this.firstName + " says bye!";
+    }
+  }
+
+  //createAnimal function
+  function createAnimal(species, verb, noise){
+    return {
+      species,
+      [verb](){
+        return noise;
+      }
+    }
+  }
+  //this one was hard...
+
+
